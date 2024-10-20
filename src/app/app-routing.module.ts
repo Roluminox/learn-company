65207ecafa497136b0e054851949cc0aa04c4f3f
Roluminox <roluminox@gmail.com>
2024-10-20 18:00:20 +0200

    team page and menu ajustements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { TeamPageComponent } from './team-page/team-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'team-page', component: TeamPageComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+// Import Page
 import { AppComponent } from './app.component';
+import { TeamPageComponent } from './team-page/team-page.component';
+// Import Component
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    TeamPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
